b907825534c812291cfc5afa0a11160ca86be67a
Thomas PINEAU <thomas.pineau93@proton.me>
2024-10-19 01:24:43 +0200

    Setup : express server and app.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,8 @@
 const http = require('http');
+const app = require('./app');
 
-const server = http.createServer((req, res) => {
-    res.end('Entry point !')
-})
+app.set('port', process.env.PORT || 3000);
 
-server.listen(process.env.PORT || 4000);
+const server = http.createServer(app)
+
+server.listen(process.env.PORT || 3000);
