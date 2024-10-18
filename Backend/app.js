36e64dee99af0b5d5579e01a7b90cf811b3b770e
Thomas PINEAU <thomas.pineau93@proton.me>
2024-10-19 01:55:49 +0200

    Updated server error management and port listening

--- Backend/app.js
+++ Backend/app.js
@@ -2,7 +2,8 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res) => {
+// Create any middleware like so
+app.use((req, res, next) => {
     res.json({ message: 'Hello World' });
 });
 
