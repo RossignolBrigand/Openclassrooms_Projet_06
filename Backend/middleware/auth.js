01791a834fbe7d881b6452e2277ceefc8d02f725
Thomas PINEAU <thomas.pineau93@proton.me>
2024-10-24 00:59:45 +0200

    changed secret token

--- Backend/middleware/auth.js
+++ Backend/middleware/auth.js
@@ -10,7 +10,7 @@ const jwt = require('jsonwebtoken');
 module.exports = (req, res, next) => {
     try {
         const token = req.headers.authorization.split('')[1];
-        const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
+        const decodedToken = jwt.verify(token, 'BfY9yLXTlOuLZZHOSS9v3vPd52kKdTGt');
         const userId = decodedToken.userId;
         req.auth = {
             userId: userId
